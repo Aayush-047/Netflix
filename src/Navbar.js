9cf95981e925d89c270de67462b9a47d82eb5ebc
Aayush <aayush.khunger217@gmail.com>
2023-08-10 19:11:16 +0530

    2nd  commit

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 // import Contact from './pages/Contact';
-import Movies from './pages/Movies';
-import TvSeries from './pages/Tvseries';
 
 export default function Navbar() {
     return (
